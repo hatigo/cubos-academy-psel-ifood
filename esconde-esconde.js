27761155fc7a2c4a32af87508142c96926ed567e
hatigo <tllucasferreira15@gmail.com>
2021-08-10 18:42:58 -0300

    Agora você me vê, Agora eu me escondi!

---
+++ esconde-esconde.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+	//seu codigo aqui
+    if (numero > 20) {
+  
+for(let i = 21; i < numero + 1; i++){
+  console.log(i)
+}
+} else {
+  console.log("VOCE CONSEGUE, MANINHO");
+}
+
+}
